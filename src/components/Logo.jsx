c92a74c22fcc76e8428c20334c563a1080867ead
pogoosstudio <dioverrdm@gmail.com>
2024-11-30 13:06:28 -0400

    Update Logo.jsx

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -6,7 +6,7 @@ function Logo() {
     <div style={{ display: "flex", alignItems: "center" }}>
       <Topgg />
       <p style={{ margin: "0 0 0 15px" }} className="site-title">
-        Top.gg Documentation
+        Pogo AI Documentación
       </p>
     </div>
   );
